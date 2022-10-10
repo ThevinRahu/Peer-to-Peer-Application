4f4b1b7a9d1233894c048755b3057b7037f7fa20
Thevin Rahubedda <thevinvr@gmail.com>
2022-10-10 23:51:32 +0530

    server and client thread changes

--- ClientGUI/MainWindow.xaml.cs
+++ ClientGUI/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +27,27 @@ namespace ClientGUI
         public MainWindow()
         {
             InitializeComponent();
+
+            Thread clock = new Thread(updateGUIClients);
+            
+        }
+
+        public void updateGUIClients()
+        {
+            while (true)
+            {
+                //thread handling
+                Thread.Sleep(2 * 60000);
+
+                //display clients
+                RestClient restClient = new RestClient("http://localhost:9987/");
+                RestRequest restRequest = new RestRequest("api/clients/", Method.Get);
+                RestResponse restResponse = restClient.Execute(restRequest);
+
+                List<Clients> clients = JsonConvert.DeserializeObject<List<Clients>>(restResponse.Content);
+
+                //display clients
+            }
         }
     }
 }
