15be144ab2d188715b930437e1540f4998bc9b46
Thevin Rahubedda <thevinvr@gmail.com>
2022-10-17 21:14:46 +0530

    website with .net core done

---
+++ Website/Models/JobPool.cs
@@ -0,0 +1,10 @@
+﻿namespace Website.Models
+{
+    public class JobPool
+    {
+        public int Id { get; set; }
+        public int job_id { get; set; }
+        public Nullable<int> finished { get; set; }
+        public string solution { get; set; }
+    }
+}
